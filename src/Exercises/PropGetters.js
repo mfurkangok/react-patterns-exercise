68e4876cac11ffd3d3e0eb395e8689731b17363e
Furkan <muhammet.gok@boun.edu.tr>
2019-06-25 17:18:36 +0300

    prop getters

--- src/Exercises/PropGetters.js
+++ src/Exercises/PropGetters.js
@@ -3,18 +3,6 @@
 import React from 'react'
 import { Switch } from '../switch'
 
-// Check out the previous usage example. How would someone pass
-// a custom `onClick` handler? It'd be pretty tricky! It'd be
-// easier to just not use the `togglerProps` prop collection!
-//
-// What if instead we exposed a function which merged props?
-// Let's do that instead. 🐨 Swap `togglerProps` with a `getTogglerProps`
-// function. It should accept props and merge the provided props
-// with the ones we need to get our toggle functionality to work
-//
-// 💰 Here's a little utility that might come in handy
-// const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
-
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
@@ -44,9 +32,6 @@ class Toggle extends React.Component {
     }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
 function Usage({
     onToggle = (...args) => console.log('onToggle', ...args),
     onButtonClick = () => console.log('onButtonClick'),
